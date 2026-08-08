8e371cb146e83356ec54dd63c5de1e40b712b973
Mustafa Roshdy <roshdyii444@gmail.com>
2026-08-08 11:44:55 +0300

    Add Course schema definitions using Pydantic

---
+++ schemas/course.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class CourseBase(BaseModel):
+    name:str
+
+
+class CourseCreate(CourseBase):
+    pass
+
+
+class CourseResponse(CourseBase):
+    id:int
+
+    model_config=ConfigDict(from_attributes=True)
