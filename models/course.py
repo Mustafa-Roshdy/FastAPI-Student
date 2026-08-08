75581b170223368fb91ac4cb12f65455d33f9dce
Mustafa Roshdy <roshdyii444@gmail.com>
2026-08-08 13:25:29 +0300

    Refactor Course and Student models for improved readability and structure

--- models/course.py
+++ models/course.py
@@ -1,11 +1,23 @@
 from core.database import base
-from sqlalchemy.orm import relationship,Mapped,mapped_column
-from sqlalchemy import Float,String,Integer,Enum,ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from sqlalchemy import String, Integer
+
 
 class Course(base):
-    __tablename__="courses"
+    __tablename__ = "courses"
 
-    id:Mapped[int]=mapped_column(Integer,autoincrement=True,primary_key=True)
-    name:Mapped[str]=mapped_column(String,nullable=False)
+    id: Mapped[int] = mapped_column(
+        Integer,
+        autoincrement=True,
+        primary_key=True
+    )
 
-    students:Mapped[list["Student"]]=relationship(back_populates="course")
+    name: Mapped[str] = mapped_column(
+        String,
+        nullable=False
+    )
+
+    students: Mapped[list["Student"]] = relationship(
+        back_populates="course",
+        cascade="all, delete-orphan"
+    )
--- models/student.py
+++ models/student.py
@@ -1,14 +1,38 @@
 from core.database import base
-from sqlalchemy.orm import relationship,Mapped,mapped_column
-from sqlalchemy import Float,String,Integer,Enum,ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from sqlalchemy import Float, String, Integer, ForeignKey
+
+
 class Student(base):
-    __tablename__="students"
+    __tablename__ = "students"
 
-    id:Mapped[int]=mapped_column(Integer,autoincrement=True,primary_key=True)
-    name:Mapped[str]=mapped_column(String(50),nullable=False)
-    email:Mapped[str]=mapped_column(String,nullable=False)
-    course_id:Mapped[int]=mapped_column(Integer,ForeignKey("courses.id"),nullable=False)
-    gpa: Mapped[float] = mapped_column(Float,nullable=False)
+    id: Mapped[int] = mapped_column(
+        Integer,
+        autoincrement=True,
+        primary_key=True
+    )
 
-    course:Mapped["Course"]=relationship(back_populates="students")
+    name: Mapped[str] = mapped_column(
+        String(50),
+        nullable=False
+    )
 
+    email: Mapped[str] = mapped_column(
+        String,
+        nullable=False
+    )
+
+    course_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("courses.id", ondelete="CASCADE"),
+        nullable=False
+    )
+
+    gpa: Mapped[float] = mapped_column(
+        Float,
+        nullable=False
+    )
+
+    course: Mapped["Course"] = relationship(
+        back_populates="students"
+    )
