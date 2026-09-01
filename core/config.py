79c173d785b1d428f8c2905601f2a7f7497ced07
Mustafa Roshdy <roshdyii444@gmail.com>
2026-09-01 10:44:48 +0300

    Update .gitignore, enhance config settings, and add requirements file

--- core/config.py
+++ core/config.py
@@ -1,13 +1,15 @@
 from pydantic_settings import BaseSettings,SettingsConfigDict
-
+import os
 
 class Settings(BaseSettings):
 
     APP_NAME:str="student App"
-    DATABASE:str="sqlite:///student.db"
+    DATABASE_URL: str  # Pydantic automatically reads DATABASE_URL from .env or system env
 
     model_config=SettingsConfigDict(
-        env_file=".env"
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"  # Prevents errors if extra variables exist in .env
     )
 
 settings=Settings()
